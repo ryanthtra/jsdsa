665e96448e07ce99309b6ca83e87ad1dc7048050
[VAIO]Ryan Transfiguracion <rtransfig@gmail.com>
2016-05-17 03:59:50 -0700

    graph_v2 -- finished breadth first search

--- ds2/graph.js
+++ ds2/graph.js
@@ -46,8 +46,8 @@ function Graph()
   // BREADTH-FIRST SEARCH ALGORITHM METHODS
   /**
    * Colors: white - node not visited
-   *         gray - node visited only
-   *         black - node visited and full explored 
+   *         gray - node discovered only
+   *         black - node visited and fully explored 
    *                  (i.e., all its neighbors visited)
    */
   var initializeColor = function()
@@ -65,6 +65,8 @@ function Graph()
   };
   /**
    * bfs
+   * @param v - the 'starting point' vertex
+   * @param callback - function we use to process a fully explored vertex
    */
   this.bfs = function(v, callback)
   {
@@ -74,6 +76,7 @@ function Graph()
     
     while (!queue.isEmpty())          // 5
     {
+      // u - the current vertex to be explored
       var u = queue.dequeue(),        // 6
           neighbors = adjList.get(u); // 7
           
@@ -95,6 +98,82 @@ function Graph()
         callback(u);
     }
   };
+  /**
+   * BFS - a breadth-first search method that stores the distances of all vertices from the parameter vertex
+   * @param v - the starting point vertex
+   * @return - an object holding the distances array and the predecessors array
+   */
+  this.BFS = function(v)
+  {
+    var color = initializeColor(),  // 2
+        queue = new Queue,          // 3
+        // d array: stores distance of index vertex from v.
+        d = [],                     //{1}
+        // pred array: the vertex from which the index vertex came
+        pred = [];                  //{2}
+    queue.enqueue(v);               // 4
+    
+    for (let i = 0; i < vertices.length; i++) //{3}
+    {
+      d[vertices[i]] = 0;           //{4}
+      pred[vertices[i]] = null;     //{5}
+    }    
+    
+    while (!queue.isEmpty())          // 5
+    {
+      // u - the current vertex to be explored
+      var u = queue.dequeue(),        // 6
+          neighbors = adjList.get(u); // 7
+          
+      // Mark DQed element as visited.
+      color[u] = 'gray';              // 8
+      // Get all the neighbors of the DQed element.
+      for (let i = 0; i < neighbors.length; i++)  // 9
+      {
+        let w = neighbors[i];       // 10
+        if (color[w] === 'white')   // 11
+        {
+          color[w] = 'grey';        // 12
+          d[w] = d[u] + 1;          //{6}
+          pred[w] = u;              //{7}
+          queue.enqueue(w);         // 13
+        }
+      }
+      // DQed element has been fully explored
+      color[u] = 'black';           // 14
+    }
+    return {
+      distances: d,
+      predecessors: pred
+    };
+  };
+  /**
+   * printShortestPaths
+   * @param v - the starting point vertex
+   */
+  this.printShortestPaths = function(v_origin)
+  {
+    var shortestPathOrigin = this.BFS(v_origin);
+    for (let i = 1; i < vertices.length; i++)
+    {
+      let toVertex = vertices[i], // Destination vertex
+          path = new Stack();
+      // From the destination vertex, we trace back to its predecessor, to the predecessor's predecessor, until we reach the origin vertex.  The 'path' stack is used to keep track of those vertices.
+      for (let v = toVertex; v !== v_origin; v = shortestPathOrigin.predecessors[v]) 
+      {
+        path.push(v);
+      }
+      path.push(v_origin);  // Since the origin ends the for loop, we have to add the vertex to the path here.
+      
+      // Create the string of the path 
+      let s = path.pop();
+      while (!path.isEmpty())
+      {
+        s += ' - ' + path.pop();
+      }
+      console.log(s);
+    }
+  }
 }
 
 function testGraph()
@@ -116,5 +195,8 @@ function testGraph()
   graph.addEdge('B','F');
   graph.addEdge('E','I');
   console.log(graph.toString());
-  graph.bfs(myVertices[0], graph.printNode);
+  graph.bfs(myVertices[3], graph.printNode);
+  var shortestPathA = graph.BFS(myVertices[0]);
+  console.log(shortestPathA);
+  graph.printShortestPaths(myVertices[3]);
 }
